47f5996b33c61a97b8ecb4884d4deebf0480521d
azka5123 <azka.thoyyib@gmail.com>
2022-11-25 00:49:13 +0700

    splash screen

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Fragment,useEffect} from "react";
+import { NativeBaseProvider, Box, Text, extendTheme } from "native-base";
+import SplashScreen from 'react-native-splash-screen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box alignItems="center" marginBottom="100px" marginTop="100px">Hello world</Box>
+      <Text>asSdd</Text>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
